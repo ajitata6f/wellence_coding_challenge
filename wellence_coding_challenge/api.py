0ba48a5958ca6ae121e0227f78bb3db46dd46e2f
Adamu Abdulaziz <ajitata6f@gmail.com>
2024-10-04 12:33:19 +0100

    Add auth router

--- wellence_coding_challenge/api.py
+++ wellence_coding_challenge/api.py
@@ -1,5 +1,8 @@
 from ninja import NinjaAPI
-from tasks.api.tasks import router as tasks_router
+
+from tasks.api.tasks import tasks_router
+from tasks.api.auth import auth_router
 
 api = NinjaAPI()
 api.add_router("/tasks/", tasks_router)
+api.add_router("/auth/", auth_router)
