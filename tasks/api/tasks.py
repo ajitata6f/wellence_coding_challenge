fd62eb8b4fb21f8ef46a174fa1425309911f2903
Adamu Abdulaziz <ajitata6f@gmail.com>
2024-10-04 12:45:30 +0100

    Implement task router

--- tasks/api/tasks.py
+++ tasks/api/tasks.py
@@ -1,9 +1,42 @@
-from django.http import HttpRequest
+from http import HTTPStatus
+
+from django.contrib.auth.decorators import login_required
+from django.http import  HttpResponse
 from ninja import Router
+from ninja.pagination import paginate
+
+from tasks.services import task_service as services
+from tasks.schemas import CreateTaskSchemaOut, CreateTaskSchemaIn, UpdateTaskSchemaIn, UpdateTaskSchemaOut, \
+    TaskSchemaOut
+from tasks.security import JWTAuth
+
+tasks_router = Router(tags=["tasks"])
+
+@tasks_router.post("/", response= {201: CreateTaskSchemaOut}, auth=JWTAuth())
+def create_task(request, payload: CreateTaskSchemaIn):
+    current_user = request.auth
+    return services.create_task(current_user, payload)
 
 
-router = Router(tags=["tasks"])
+@tasks_router.put("/{int:task_id}" , response= {200: UpdateTaskSchemaOut}, auth=JWTAuth())
+@login_required
+def update_task(request, task_id: int, payload: UpdateTaskSchemaIn):
+    payload.id = task_id
+    return services.update_task(payload)
 
-@router.get("/")
-def index(request: HttpRequest):
-    return {"message": "Hello World"}
+
+@tasks_router.delete("/{int:task_id}")
+def delete_task(request, task_id: int):
+    services.delete_task(task_id=task_id)
+    return HttpResponse(status=HTTPStatus.NO_CONTENT)
+
+
+@tasks_router.get("/{int:task_id}", response=TaskSchemaOut)
+def get_task(request, task_id: int):
+    return services.get_task(task_id)
+
+
+@tasks_router.get("/", response=list[TaskSchemaOut])
+@paginate
+def list_tasks(request):
+    return services.list_tasks()
